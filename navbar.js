22c0a41c9ff7204d2bebc71270197ca45a50d542
Mahmoud Shalabi <mahmouds2006@gmail.com>
2025-11-19 12:28:14 -0500

    Reorganize sections: Projects first, Skills before Contact. Update email functionality to use clipboard.

--- navbar.js
+++ navbar.js
@@ -39,11 +39,11 @@ function createNavbar(isHomePage = false) {
               <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd"></path>
             </svg>
           </a>
-          <a href="mailto:mahmouds2006@gmail.com" class="p-2 rounded-lg hover:bg-gray-100 transition" aria-label="Email">
+          <button id="emailButtonDesktop" class="p-2 rounded-lg hover:bg-gray-100 transition cursor-pointer" aria-label="Email">
             <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
             </svg>
-          </a>
+          </button>
         </div>
         
         ${isHomePage ? `
@@ -86,11 +86,11 @@ function createNavbar(isHomePage = false) {
                 <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd"></path>
               </svg>
             </a>
-            <a href="mailto:mahmouds2006@gmail.com" class="p-2 rounded-lg hover:bg-gray-100 transition">
+            <button id="emailButtonMobile" class="p-2 rounded-lg hover:bg-gray-100 transition cursor-pointer">
               <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
               </svg>
-            </a>
+            </button>
           </div>
         </div>
       </div>
